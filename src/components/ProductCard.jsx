3627ea6d08aa209a973fa651a0c48fade6c3c402
MonssterCore <monssteremottion@gmail.com>
2025-07-31 13:19:12 -0400

    add

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { getTagColor } from "../helpers/tagscolors";
 
 const ProductCard = ({ product }) => {
   const { _id, name, brand, price, image, tags, status } = product;
@@ -10,18 +11,32 @@ const ProductCard = ({ product }) => {
   };
 
   return (
-    <div
+      <div
       onClick={handleClick}
-      className="border rounded-xl shadow-md p-4 hover:shadow-lg transition cursor-pointer"
+      className="shadow-md hover:shadow-lg transition cursor-pointer text-sm"
     >
-      <img
-        src={image}
-        alt={name}
-        className="w-full h-48 object-cover rounded-md mb-2"
-      />
-      <h2 className="text-lg font-semibold">{name}</h2>
-      <p className="text-sm text-gray-500">{brand}</p>
-      <p className="mt-1 font-bold text-green-700">${price}</p>
+      <div className="relative w-full h-[400px] mb-2">
+        <div className="absolute top-2 left-2 space-y-1 z-10 flex flex-raw gap-1">
+          {tags?.map((tag, index) => (
+            <span
+              key={index}
+              className={` bg-opacity-60 ${getTagColor(tag.name)} text-black text-xs px-2 py-0.5 rounded-md`}
+            >
+              {tag.name}
+            </span>
+          ))}
+        </div>
+        <img
+          src={image}
+          alt={name}
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="p-3">
+        <h2 className="text-ms font-semibold">{name}</h2>
+        <p className="text-gray-500">{brand}</p>
+        <p className="mt-1">Desde $<strong>{price}</strong></p>
+      </div>
     </div>
   );
 };
