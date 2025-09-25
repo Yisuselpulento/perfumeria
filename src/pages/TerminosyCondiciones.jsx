d3f820a90a64e6bcb555614160f0c62495c7747e
MonssterCore <monssteremottion@gmail.com>
2025-09-25 15:57:31 -0300

    fixed

--- src/pages/TerminosyCondiciones.jsx
+++ src/pages/TerminosyCondiciones.jsx
@@ -7,7 +7,7 @@ const TerminosyCondiciones = () => {
       <section className="space-y-2">
         <h3 className="font-semibold">PRIMERO: GENERALIDADES</h3>
         <p>
-          El presente documento regula y describe los términos y condiciones para acceder y usar los servicios de www.glamesence.com.
+          El presente documento regula y describe los términos y condiciones para acceder y usar los servicios de www.moriakuma.cl.
         </p>
         <p>
           El presente documento serán entendidos y aceptados por cada persona que acceda al sitio web descrito.
@@ -21,14 +21,14 @@ const TerminosyCondiciones = () => {
         <h3 className="font-semibold">SEGUNDO: CONTRATACION DE SERVICIOS</h3>
         <p>Para contratar servicios o realizar una compra en nuestro sitio web, deberá seguir los siguientes pasos:</p>
         <ol className="list-decimal list-inside space-y-1">
-          <li>Ingresa a www.glamesence.com</li>
-          <li>Selecciona el o los productos que le interesan y agrégalos a su “carrito de compra” o realizar una compra rápida con la opción "comprar ahora".</li>
-          <li>Rellena los datos para despachar tu pedido (Nombre, Rut, Teléfono, Comuna, Región, Medio de transporte).</li>
+          <li>Ingresa a www.moriakuma.cl</li>
+          <li>Selecciona el o los productos que le interesan y agrégalos a su “carrito de compra”.</li>
+          <li>Rellena los datos para despachar tu pedido (Nombre, Rut, Teléfono, Comuna, Región).</li>
           <li>Seleccione el medio de pago (Web pay).</li>
           <li>Si quieres iniciar sesión para guardar tus datos, direcciones, compras u otros, selecciona la opción entrar.</li>
           <li>Selecciona la opción: ¿No posee una cuenta? Crear una.</li>
-          <li>Rellena los datos solicitados y listo, ya eres usuario de Glam Esence.</li>
-          <li>Si ya tienes una cuenta en Glam Esence rellena el correo y contraseña.</li>
+          <li>Rellena los datos solicitados y listo, ya eres usuario de Mori Akuma.</li>
+          <li>Si ya tienes una cuenta en Mori Akuma rellena el correo y contraseña.</li>
           <li>Posterior a tu compra, se enviará la información de tu pedido al correo electrónico que registraste.</li>
         </ol>
       </section>
@@ -44,21 +44,21 @@ const TerminosyCondiciones = () => {
           El pago con tarjetas de crédito o débito se realizará a través del sistema WebPay, sistema de pago electrónico que se encarga de hacer el cargo automático a la cuenta bancaria de la persona.
         </p>
         <p>
-          Los usuarios aceptan que estos medios de pago pertenecen a una empresa externa, independientes y no vinculadas a Glam Esence, por lo que la prestación de servicios y funcionamiento de esta será de exclusiva responsabilidad de las empresas proveedoras de estos servicios y en ningún caso de Glam Esence.
+          Los usuarios aceptan que estos medios de pago pertenecen a una empresa externa, independientes y no vinculadas a Mori Akuma, por lo que la prestación de servicios y funcionamiento de esta será de exclusiva responsabilidad de las empresas proveedoras de estos servicios y en ningún caso de Mori Akuma.
         </p>
       </section>
 
       <section className="space-y-2">
         <h3 className="font-semibold">CUARTO: DESPACHO DE PRODUCTOS</h3>
         <p>
-          Los despachos y entregas de los productos adquiridos en Glam Esence podrán ser escogidas por el usuario según su requerimiento. Los despachos podrán ser a todo Chile.
+          Los despachos y entregas de los productos adquiridos en Mori Akuma podrán ser escogidas por el usuario según su requerimiento. Los despachos podrán ser a todo Chile.
         </p>
       </section>
 
       <section className="space-y-2">
         <h3 className="font-semibold">QUINTO: COMUNICACIONES</h3>
         <p>
-          Para la comunicación de un usuario con la empresa Glam Esence ya sea; una consulta, sugerencia, reclamo u otro, se podrá realizar mediante el correo <a href="mailto:glamessence1997@gmail.com" className="text-blue-600 underline">glamessence1997@gmail.com</a> o escribirnos por nuestras redes sociales.
+          Para la comunicación de un usuario con la empresa Mori Akuma ya sea; una consulta, sugerencia, reclamo u otro, se podrá realizar mediante el correo <a href="mailto:glamessence1997@gmail.com" className="text-blue-600 underline">moriakuma.d@gmail.com</a> o escribirnos por nuestras redes sociales.
         </p>
       </section>
 
@@ -75,7 +75,7 @@ const TerminosyCondiciones = () => {
           El sitio web tomará las medidas necesarias para resguardar la seguridad de los datos entregados y clave secreta u otros que la empresa encuentre correspondiente.
         </p>
         <p>
-          Los usuarios son exclusivamente responsables del cliente que los utiliza, Glam Esence no responderá por el mal uso, pérdida o uso no autorizado del código de validación u otro que mantenga relación directa o indirecta con el usuario.
+          Los usuarios son exclusivamente responsables del cliente que los utiliza, Mori Akuma no responderá por el mal uso, pérdida o uso no autorizado del código de validación u otro que mantenga relación directa o indirecta con el usuario.
         </p>
       </section>
 
@@ -98,12 +98,12 @@ const TerminosyCondiciones = () => {
       <section className="space-y-2">
         <h3 className="font-semibold">NOVENO: RESPONSABILIDADES</h3>
         <p>
-          Glam Esence no responderá en ningún caso por las siguientes condiciones o acciones que se describen a continuación:
+          Mori Akuma no responderá en ningún caso por las siguientes condiciones o acciones que se describen a continuación:
         </p>
         <ul className="list-disc list-inside space-y-1">
           <li>Posibles daños y/o perjuicios por el funcionamiento de las herramientas de búsqueda o por posibles errores del sitio.</li>
           <li>Mal uso, pérdida o uso no autorizado de su clave de ingreso al sitio web.</li>
-          <li>Cualquier contenido del sitio web al que puedan acceder con o sin autorización de Glam Esence.</li>
+          <li>Cualquier contenido del sitio web al que puedan acceder con o sin autorización de Mori Akuma.</li>
         </ul>
       </section>
     </div>
